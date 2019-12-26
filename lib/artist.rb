f933d6c402a37136a94ee5f5bd716d73933f79ce
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-26 02:06:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,31 @@
-
 class Artist
   attr_accessor :name
+
   @@song_count = 0
+
   def initialize(name)
     @name = name
-    @songs = [] #empty collection of songs
-  end
-
-  def songs
-    @songs
+    @songs = []
   end
 
   def add_song(song)
-    @songs << song
+    self.songs << song
     song.artist = self
-    @@song_count += 1
+    @@song_count +=1
   end
 
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    @songs << song
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
     song.artist = self
-    @@song_count += 1
+    @@song_count +=1
+  end
+
+  def songs
+    @songs 
   end
 
   def self.song_count
     @@song_count
   end
-
 end
